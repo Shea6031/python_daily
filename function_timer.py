8ddeea2bb1478b287533ee1cda00998d42054dad
Shea6031 <xj11306031@126.com>
2018-04-09 13:59:32 +0800

    func timer

---
+++ function_timer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  9 11:12:50 2018
+
+@author: xj2sgh
+"""
+
+import time
+from functools import wraps
+
+def fn_timer(function):
+    @wraps(function)
+    def function_timer(*args, **kwargs):
+        t0 = time.time()
+        result = function(*args, **kwargs)
+        t1 = time.time()
+        print("Total time running %s:%s seconds" %(function.__name__, str(t1-t0)))
+        return result
+    return function_timer
+
+@fn_timer
+def sumfunction():
+    sum = 0
+    for i in range(10):
+      sum+=i
+    print(sum)
+    
+if __name__ =="__main__":
+    myfunction()
